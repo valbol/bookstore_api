ef09eb8df0456a1d515ecb6f6a17be5d1afd3eb0
Valery_Boltavsky <valery.boltavsky@checkmarx.com>
2024-04-25 21:45:23 +0300

    init

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+
+
+const createApp = async () => {
+  const app: Application = express();
+
+  const getCorsAllowedOrigins = () => {
+    if (process.env.CLIENT_ALLOWED_ORIGINS) {
+      const origins = process.env.CLIENT_ALLOWED_ORIGINS.split(',').map((item) => item.trim());
+      return [new RegExp(process.env.CLIENT_ALLOWED_ORIGIN_REGEX), ...origins];
+    }
+    return [new RegExp(process.env.CLIENT_ALLOWED_ORIGIN_REGEX)];
+  };
+
+  const corsOptions = {
+    origin: getCorsAllowedOrigins(),
+    credentials: true,
+  };
+
+  app.use(cors(corsOptions));
+  app.use(express.json({ limit: '5mb' }));
+  app.use(express.urlencoded({ limit: '5mb', extended: true }));
+ 
+  return app;
+};
+
+export default createApp;
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import createApp from './app';
+import dotenv from 'dotenv';
+import logger from './logger';
+
+const initializeServer = async () => {
+  dotenv.config();
+  console.log(process.env)
+  const app = await createApp();
+  const port = Number(process.env.SERVER_PORT) || 3000;
+
+  app.listen(port, () => {
+    logger.info(`App is running on port ${port} in ${app.get('env')} mode`);
+    logger.info('Press CTRL-C to stop');
+  });
+};
+
+initializeServer()
+  .then(() => {
+    logger.info('server initialized successfully');
+  })
+  .catch((error) => {
+    logger.error(`server initialization error: ${error}`);
+  });
