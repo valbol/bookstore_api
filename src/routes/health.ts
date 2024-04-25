30944ddb04527cc1bdb4f777c96a35178f8689a5
Valery_Boltavsky <valery.boltavsky@checkmarx.com>
2024-04-25 21:57:09 +0300

    eslint and prittier

---
+++ src/routes/health.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from "express";
+
+const router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+  const commit = process.env.GIT_HASH || "Im alive";
+
+  res.send({ latest_commit_sha: commit });
+});
+export default router;
---
+++ src/types/book.ts
@@ -0,0 +1,6 @@
+export enum Genre {
+  Drama = "Drama",
+  Action = "Action",
+  Romance = "Romance",
+  Horror = "Horror",
+}
